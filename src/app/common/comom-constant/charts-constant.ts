23995ae9c55588e3dc46a8647296bb661cdd4c12
corenzhiliang <1176033349@qq.com>
2018-10-30 08:36:50 +0800

    调整

--- src/app/common/comom-constant/charts-constant.ts
+++ src/app/common/comom-constant/charts-constant.ts
@@ -1,3 +1,92 @@
-export const chartConstant = {
+import { graphic } from 'echarts';
 
+/**
+ * 图标配置
+ *
+ */
+// 柱状图;
+export const chartBarConstant = {
+      title: {
+        text: '新七天总仓库每件商品出货量',
+      },
+      xAxis: {
+        // data: dataAxis,
+        axisLabel: {
+          inside: true,
+          textStyle: {
+            color: '#fff',
+          },
+          // position: 'top'
+        },
+        axisTick: {
+          show: true
+        },
+        axisLine: {
+          show: true
+        },
+        z: 10
+      },
+      yAxis: {
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          textStyle: {
+            color: '#666'
+          }
+        }
+      },
+      dataZoom: [
+        {
+          type: 'inside'
+        }
+      ],
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow',
+        },
+      },
+      series: [
+        { // For shadow
+          name: '商品信息',
+          type: 'bar',
+          itemStyle: {
+            normal: { color: 'rgba(0,0,0,0.05)' }
+          },
+          barGap: '-100%',
+          barCategoryGap: '40%',
+        //   data: dataShadow,
+          animation: false
+        },
+        {
+          type: 'bar',
+          itemStyle: {
+            normal: {
+              color: new graphic['LinearGradient'](
+                0, 0, 0, 1,
+                [
+                  { offset: 0, color: '#83bff6' },
+                  { offset: 0.5, color: '#188df0' },
+                  { offset: 1, color: '#188df0' }
+                ]
+              )
+            },
+            emphasis: {
+              color: new graphic['LinearGradient'](
+                0, 0, 0, 1,
+                [
+                  { offset: 0, color: '#2378f7' },
+                  { offset: 0.7, color: '#2378f7' },
+                  { offset: 1, color: '#83bff6' }
+                ]
+              )
+            }
+          },
+        //   data: data
+        }
+      ]
 };
--- src/app/store/store-cost/store-cost.component.ts
+++ src/app/store/store-cost/store-cost.component.ts
@@ -25,7 +25,7 @@ export class StoreCostComponent extends ShareCommon implements OnInit {
   dataList: any;
   // 整体成本走势
   options: any;
-  initOpts: any;
+  autoResize: any;
   detectEventChanges = true;
   date: string;
   searchTypeList = [
@@ -50,103 +50,33 @@ export class StoreCostComponent extends ShareCommon implements OnInit {
       {name: '总仓库', checked: true},
       {name: '仓库1', checked: false}
     ];
-    const dataAxis = ['奶茶', '杯子', '大碗', '凉面碗', '奶精', '珍珠', '苹果', '西瓜', '柠檬', '奶茶杯'];
-    const data = [220, 182, 191, 234, 290, 330, 310, 123, 442, 321];
-    const dataShadow = [];
-
-    for (let i = 0; i < data.length; i++) {
-      dataShadow.push(data[i]);
-    }
-    this.initOpts = {
-      renderer: 'svg',
-      height: 400
-    };
-    this.options = {
-      title: {
-        text: '新七天总仓库每件商品出货量',
-      },
-      xAxis: {
-        data: dataAxis,
-        axisLabel: {
-          inside: true,
-          textStyle: {
-            color: '#fff',
-          },
-          // position: 'top'
-        },
-        axisTick: {
-          show: true
-        },
-        axisLine: {
-          show: true
-        },
-        z: 10
-      },
-      yAxis: {
-        axisLine: {
-          show: false
-        },
-        axisTick: {
-          show: false
-        },
-        axisLabel: {
-          textStyle: {
-            color: '#666'
-          }
-        }
-      },
-      dataZoom: [
-        {
-          type: 'inside'
-        }
-      ],
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'shadow',
-        },
-      },
-      series: [
-        { // For shadow
-          name: '商品信息',
-          type: 'bar',
-          itemStyle: {
-            normal: { color: 'rgba(0,0,0,0.05)' }
-          },
-          barGap: '-100%',
-          barCategoryGap: '40%',
-          data: dataShadow,
-          animation: false
-        },
-        {
-          type: 'bar',
-          itemStyle: {
-            normal: {
-              color: new graphic['LinearGradient'](
-                0, 0, 0, 1,
-                [
-                  { offset: 0, color: '#83bff6' },
-                  { offset: 0.5, color: '#188df0' },
-                  { offset: 1, color: '#188df0' }
-                ]
-              )
-            },
-            emphasis: {
-              color: new graphic['LinearGradient'](
-                0, 0, 0, 1,
-                [
-                  { offset: 0, color: '#2378f7' },
-                  { offset: 0.7, color: '#2378f7' },
-                  { offset: 1, color: '#83bff6' }
-                ]
-              )
-            }
-          },
-          data: data
-        }
-      ]
-    };
+   // 总成本走势
+  this.autoResize = true;
+  this.options = {
+    title: {
+      text: '库房每月总成本走势'
+    },
+    xAxis: {
+      type: 'category',
+      data: ['一月', '二月', '三月', '四月', '五月', '六月', '7月', '8月', '9月', '10月']
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        animation: false
+      }
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [{
+      data: [820, 932, 901, 934, 1290, 1330, 1320, 1500, 1630, 1750, 1840 ],
+      type: 'line'
+    }]
+  };
   }
+
+
   ngOnInit() {
     this.searchStream.next();
   }
